cfe4df36badff49023b006a8e6a0b8db6c243e18
Anudeep <asa.anudeep4@gmail.com>
2020-01-05 12:45:35 +0530

    added interactions model

---
+++ src/models/interaction.js
@@ -0,0 +1,34 @@
+const helper = require('../helpers/helper');
+
+class InteractionRequest {
+
+  constructor(request) {
+    this.method = request.method;
+    this.path = request.path;
+  }
+
+}
+
+class InteractionResponse {
+
+  constructor(response) {
+    this.status = response.status;
+    this.headers = response.headers;
+    this.body = response.body
+  }
+
+}
+
+class Interaction {
+
+  constructor(rawInteraction) {
+    this.id = helper.getRandomId();
+    this.port = rawInteraction.port || 3000;
+    this.rawInteraction = rawInteraction;
+    this.withRequest = new InteractionRequest(rawInteraction.withRequest);
+    this.willRespondWith = new InteractionResponse(rawInteraction.willRespondWith);
+  }
+
+}
+
+module.exports = Interaction;
