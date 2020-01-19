a702151dc1f8b7e5fbadffd68e30a2e50d611625
Anudeep <asa.anudeep4@gmail.com>
2020-01-19 13:51:55 +0530

    added js docs for HTTP methods in index

--- src/index.js
+++ src/index.js
@@ -46,30 +46,102 @@ const pactum = {
     return spec.addInteraction(interaction);
   },
 
+  /**
+   * The GET method requests a representation of the specified resource. Requests using GET should only retrieve data.
+   * @param {string} url - HTTP url
+   * @example
+   * await pactum
+   *  .get('https://jsonplaceholder.typicode.com/posts')
+   *  .withQuery('postId', 1)
+   *  .expectStatus(200)
+   *  .expectJsonLike({
+   *    userId: 1,
+   *    id: 1
+   *   })
+   *  .toss();
+   */
   get(url) {
     return new Spec(server).get(url);
   },
 
+  /**
+   * The HEAD method asks for a response identical to that of a GET request, but without the response body.
+   * @param {string} url - HTTP url
+   */
   head(url) {
     return new Spec(server).head(url);
   },
 
+  /**
+   * The OPTIONS method is used to describe the communication options for the target resource.
+   * @param {string} url - HTTP url
+   */
   options(url) {
     return new Spec(server).options(url);
   },
 
+  /**
+   * The PATCH method is used to apply partial modifications to a resource.
+   * @param {string} url - HTTP url
+   * @example
+   * await pactum
+   *  .patch('https://jsonplaceholder.typicode.com/posts/1')
+   *  .withJson({
+   *    title: 'foo'
+   *  })
+   *  .expectStatus(200)
+   *  .toss();
+   */
   patch(url) {
     return new Spec(server).patch(url);
   },
 
+  /**
+   * The POST method is used to submit an entity to the specified resource, often causing a change in state or side effects on the server.
+   * @param {string} url - HTTP url
+   * @example
+   * await pactum
+   *  .post('https://jsonplaceholder.typicode.com/posts')
+   *  .withJson({
+   *    title: 'foo',
+   *    body: 'bar',
+   *    userId: 1
+   *  })
+   *  .expectStatus(201)
+   *  .toss();
+   */
   post(url) {
     return new Spec(server).post(url);
   },
 
+  /**
+   * The PUT method replaces all current representations of the target resource with the request payload.
+   * @param {string} url - HTTP url
+   * @example
+   * await pactum
+   *  .put('https://jsonplaceholder.typicode.com/posts/1')
+   *  .withJson({
+   *    id: 1,
+   *    title: 'foo',
+   *    body: 'bar',
+   *    userId: 1
+   *  })
+   *  .expectStatus(200)
+   *  .toss();
+   */
   put(url) {
     return new Spec(server).put(url);
   },
 
+  /**
+   * The DELETE method deletes the specified resource.
+   * @param {string} url - HTTP url
+   * @example
+   * await pactum
+   *  .delete('https://jsonplaceholder.typicode.com/posts/1')
+   *  .expectStatus(200)
+   *  .toss();
+   */
   delete(url) {
     return new Spec(server).delete(url);
   }
