c139622ac325335f18dc93d09af0fa4a985d47fb
Anudeep <asa.anudeep4@gmail.com>
2020-01-25 15:37:07 +0530

    added new function registerAllRoutes in server

--- src/models/server.js
+++ src/models/server.js
@@ -17,40 +17,24 @@ class Server {
       } else {
         const app = express();
         app.use(express.json());
-        app.all('/*', (req, res) => {
-          const mock = this.mockMap.get(req.app.port);
-          const { interactions, defaultMockInteractions } = mock;
-          let interactionExercised = false;
-          let interaction = helper.getValidInteraction(req, interactions);
-          if (!interaction) {
-            interaction = helper.getValidInteraction(req, defaultMockInteractions);
-          }
-          if (interaction) {
-            interaction.exercised = true;
-            interactionExercised = true;
-            res.set(interaction.willRespondWith.headers);
-            res.status(interaction.willRespondWith.status);
-            res.send(interaction.willRespondWith.body);
-          }
-          if (!interactionExercised) {
-            console.log('PACTUM', 'Interaction not found for');
-            console.log('PACTUM', req.method, req.path);
-            console.log('PACTUM', 'Query', req.query);
-            console.log('PACTUM', 'Body', req.body);
-            res.status(404);
-            res.send('Interaction Not Found');
-          }
-        });
+        registerAllRoutes(this, app);
         const server = app.listen(port, () => {
           console.log('PACTUM mock server is listening on port', port);
           app.port = port;
-          this.mockMap.set(port, {
-            app,
-            server,
-            running: true,
-            interactions: new Map(),
-            defaultMockInteractions: new Map()
-          });
+          if (this.mockMap.has(port)) {
+            const mockApp = this.mockMap.get(port);
+            mockApp.app = app;
+            mockApp.server = server;
+            mockApp.running = true;
+          } else {
+            this.mockMap.set(port, {
+              app,
+              server,
+              running: true,
+              interactions: new Map(),
+              defaultInteractions: new Map()
+            });
+          }
           resolve();
         });
       }
@@ -90,7 +74,12 @@ class Server {
     const port = interaction.port;
     if (this.mockMap.has(port)) {
       const mock = this.mockMap.get(port);
-      mock.defaultMockInteractions.set(id, interaction);
+      mock.defaultInteractions.set(id, interaction);
+    } else {
+      const interactions = new Map();
+      const defaultInteractions = new Map();
+      defaultInteractions.set(id, interaction);
+      this.mockMap.set(port, { interactions, defaultInteractions });
     }
   }
 
@@ -104,7 +93,7 @@ class Server {
   removeDefaultMockInteraction(id, port = config.mock.port) {
     if (this.mockMap.has(port)) {
       const mock = this.mockMap.get(port);
-      mock.defaultMockInteractions.delete(id);
+      mock.defaultInteractions.delete(id);
     }
   }
 
@@ -118,7 +107,7 @@ class Server {
   removeDefaultMockInteractions(port = config.mock.port) {
     if (this.mockMap.has(port)) {
       const mock = this.mockMap.get(port);
-      mock.defaultMockInteractions.clear();
+      mock.defaultInteractions.clear();
     }
   }
 
@@ -126,10 +115,43 @@ class Server {
     for (const [port, mock] of this.mockMap.entries()) {
       console.log(`Removing all interactions for ${port}`);
       mock.interactions.clear();
-      mock.defaultMockInteractions.clear();
+      mock.defaultInteractions.clear();
     }
   }
 
+}
+
+/**
+ * registers all routes for interactions
+ * @param {Server} server - server object
+ * @param {Express} app - express app object
+ */
+function registerAllRoutes(server, app) {
+  app.all('/*', (req, res) => {
+    const mock = server.mockMap.get(req.app.port);
+    const { interactions, defaultInteractions } = mock;
+    let interactionExercised = false;
+    let interaction = helper.getValidInteraction(req, interactions);
+    if (!interaction) {
+      interaction = helper.getValidInteraction(req, defaultInteractions);
+    }
+    if (interaction) {
+      interaction.exercised = true;
+      interactionExercised = true;
+      res.set(interaction.willRespondWith.headers);
+      res.status(interaction.willRespondWith.status);
+      res.send(interaction.willRespondWith.body);
+    }
+    if (!interactionExercised) {
+      console.log();
+      console.log('PACTUM', 'Interaction not found for');
+      console.log('PACTUM', req.method, req.path);
+      console.log('PACTUM', 'Query', req.query);
+      console.log('PACTUM', 'Body', req.body);
+      res.status(404);
+      res.send('Interaction Not Found');
+    }
+  });
 }
 
 module.exports = Server;
