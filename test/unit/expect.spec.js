3e0e17e704ddc83dc669d379904a78223c6c51b3
Anudeep <asa.anudeep4@gmail.com>
2020-02-29 15:15:11 +0530

    updated version

---
+++ test/unit/expect.spec.js
@@ -0,0 +1,16 @@
+const Expect = require('../../src/models/expect');
+
+describe.only('Expect', () => {
+
+  it('validate status', () => {
+    const ex = new Expect();
+    ex._validateStatus({});
+  });
+
+  it('validate headers', () => {
+    const ex = new Expect();
+    ex.headers.push({key: 'c', value: undefined });
+    ex._validateHeaders({ headers: { c: 'ss'}});
+  });
+
+});
