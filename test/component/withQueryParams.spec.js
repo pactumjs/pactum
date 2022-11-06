d008ea4e1586d47699b0304975aefcd421e1af52
Anudeep <asa.anudeep4@gmail.com>
2022-11-06 10:25:54 +0530

    feat: support query params without values

---
+++ test/component/withQueryParams.spec.js
@@ -0,0 +1,70 @@
+const pactum = require('../../src/index');
+
+describe('withQueryParams', () => {
+
+  describe('should process', () => {
+    
+    it('without value', async () => {
+      await pactum.spec()
+        .useInteraction({
+          request: {
+            method: 'GET',
+            path: '/api/query',
+            queryParams: {
+              refresh: ''
+            }
+          },
+          response: {
+            status: 200
+          }
+        })
+        .get('http://localhost:9393/api/query')
+        .withQueryParams('refresh')
+        .expectStatus(200);
+    });
+
+    it('with multiple query params without values', async () => {
+      await pactum.spec()
+        .useInteraction({
+          request: {
+            method: 'GET',
+            path: '/api/query',
+            queryParams: {
+              refresh: '',
+              id: ''
+            }
+          },
+          response: {
+            status: 200
+          }
+        })
+        .get('http://localhost:9393/api/query')
+        .withQueryParams('refresh')
+        .withQueryParams('id')
+        .expectStatus(200);
+    });
+
+    it('with multiple query params where one without value', async () => {
+      await pactum.spec()
+        .useInteraction({
+          request: {
+            method: 'GET',
+            path: '/api/query',
+            queryParams: {
+              refresh: '',
+              id: '1'
+            }
+          },
+          response: {
+            status: 200
+          }
+        })
+        .get('http://localhost:9393/api/query')
+        .withQueryParams('refresh')
+        .withQueryParams('id', '1')
+        .expectStatus(200);
+    });
+  
+  });
+  
+});
