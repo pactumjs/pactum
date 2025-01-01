9cd4e83b2af361b9b5bfbdf714de3d3d7affa395
Anudeep <asa.anudeep4@gmail.com>
2025-01-01 20:41:28 +0530

    chore: after response error (#395)

--- src/exports/events.js
+++ src/exports/events.js
@@ -1,19 +1,14 @@
 const { EventEmitter } = require('events');
 
-/**
- * @deprecated
- */
-const events = new EventEmitter();
-
 const pactumEvents = new EventEmitter();
 
 const EVENT_TYPES = {
   BEFORE_REQUEST: "BEFORE_REQUEST",
   AFTER_RESPONSE: "AFTER_RESPONSE",
+  AFTER_RESPONSE_ERROR: "AFTER_RESPONSE_ERROR",
 }
 
 module.exports = {
-  events,
   pactumEvents,
   EVENT_TYPES
 }
